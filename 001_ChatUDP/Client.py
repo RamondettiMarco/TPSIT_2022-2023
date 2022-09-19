d4505988b681625f002155955b61421de867290f
Marco Ramondetti <marco.ramondetti@itiscuneo.eu>
2022-09-19 10:23:02 +0200

    ottimizzazione chat UDP

--- 001_ChatUDP/Client.py
+++ 001_ChatUDP/Client.py
@@ -3,18 +3,22 @@ from socket import AF_INET, SOCK_DGRAM, socket
 #PORT = 5000
 #BROADCAST = "192.168.95.255"
 
-def chatClient():
+def valori_input_client():
     ip = input("Inserire l'indirizzo IP: ")
-    porta = input("Inserire la porta: ")
+    porta = int(input("Inserire la porta: "))
     username = input("Inserire il tuo username: ")
+    
+    return ip, porta, username
+
+def chatClient(ip, porta, username):
     with socket(AF_INET, SOCK_DGRAM) as s:
         while True:
             msg = input("Inserire il messaggio da inviare: ")
             msg = f"l'utente {username.upper()} ha scritto: {msg}"
             msg = msg.encode('utf8')
             #s.sendto(msg, (BROADCAST, PORT))
-            s.sendto(msg, (ip, int(porta)))
+            s.sendto(msg, (ip, porta))
             
-
 if __name__ == "__main__":
-    chatClient()
+    ip, porta, username = valori_input_client()
+    chatClient(ip, porta, username)
--- 001_ChatUDP/Server.py
+++ 001_ChatUDP/Server.py
@@ -3,23 +3,27 @@ from socket import socket, AF_INET, SOCK_DGRAM
 BUFFER_SIZE = 1024
 #tipo "bytes" -> array di byte
 #codifica per esempio da str a bytes bisogna trasformarla in UNICODE esempio codifica utf8
-HOST = '0.0.0.0'
+#HOST = '0.0.0.0'
 #posso mettere: 127.0.0.1 -> localhost
 #posso mettere: il mio indirizzo ip -> es. 192.168.95.141 -> solo sulla rete
 #oppure 0.0.0.0 -> ascolta tutto
-PORT = 5000 #basta che sia sopra 1024
+#PORT = 5000 #basta che sia sopra 1024
 
-def chatServer():
+def valori_input_server():
+    host = input("Inserire l'indirizzo IP: ")
+    porta = int(input("Inserire la porta: "))
+    
+    return host, porta
+
+def chatServer(host, port):
     with socket(AF_INET, SOCK_DGRAM) as s:
-        s.bind((HOST, PORT))
+        s.bind((host, port))
         while True:
             msg = s.recvfrom(BUFFER_SIZE) #arriva un bytes quindi msg è di tipo bytes, 
                                           #bisogna decodificarlo per avere una str)
             msg = msg[0].decode('utf8')
             print(msg)
             
-    
-    
-
 if __name__ == "__main__":
-    chatServer()
+    host, porta = valori_input_server()
+    chatServer(host, porta)
