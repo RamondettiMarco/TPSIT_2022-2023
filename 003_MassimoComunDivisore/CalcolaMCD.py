335e13192608e0fa31800ffd31e87ba86b9c20e1
Marco Ramondetti <marco.ramondetti@itiscuneo.eu>
2022-10-24 09:43:22 +0200

    Calcolo del massimo comun divisore

---
+++ 003_MassimoComunDivisore/CalcolaMCD.py
@@ -0,0 +1,6 @@
+import math
+
+n1 = int(input("inserisci il primo numero: "))
+n2 = int(input("inserisci il secondo numero: "))
+
+print(f"Il massimo comun divisore tra {n1} e {n2} è {math.gcd(n1,n2)}")
