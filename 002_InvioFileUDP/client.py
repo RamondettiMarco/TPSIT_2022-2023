202586d1e0c0eca460ea52968961a28831bbdd2b
RamondettiMarco <marco.ramondetti@itiscuneo.eu>
2022-12-13 09:43:25 +0100

    Invio di un file con UDP

--- 002_InvioFileUDP/client.py
+++ 002_InvioFileUDP/client.py
@@ -1,14 +1,14 @@
 from socket import AF_INET, SOCK_DGRAM, socket
 from packet import Packet
 
-
+BUFFER_SIZE = 4096
 
 def valori_input_client():
     ip = input("Inserire l'indirizzo IP: ")
     porta = int(input("Inserire la porta: "))
-    #username = input("Inserire il tuo username: ")
+    file = input("Inserire il tuo file: ")
     
-    return ip, porta #, username
+    return ip, porta, file
 
 receiver = ("127.0.0.1", 5000)
 
@@ -18,7 +18,7 @@ def Client():
             s.sendto(Packet(Packet.NEW_FILE, b'').to_bytes(), receiver)
             data = True
             while data:
-                data = f.read(4096)
+                data = f.read(BUFFER_SIZE)
                 if data:
                     s.sendto(Packet(Packet.GO_ON, data).to_bytes(), receiver)
                     #time.sleep(0.001)
@@ -26,5 +26,5 @@ def Client():
             s.sendto(Packet(Packet.END_FILE, b'').to_bytes(), receiver)
             
 if __name__ == "__main__":
-    #ip, porta, username = valori_input_client()
+    #ip, porta, file = valori_input_client()
     Client()
--- 002_InvioFileUDP/server.py
+++ 002_InvioFileUDP/server.py
@@ -19,22 +19,19 @@ def Server():
     with socket(AF_INET, SOCK_DGRAM) as s:
         s.bind(('0.0.0.0', 5000))
         file = []
-
+        
         while True:
             msg = s.recvfrom(MAX_SIZE)
             packet = Packet.from_bytes(msg[0])
             if packet.status == Packet.NEW_FILE:
                 file = []
-
-            if packet.data and len(packet.data) > 0:
+            elif packet.data and len(packet.data) > 0:
                 file.append(packet.data)
-
-            if packet.status == Packet.END_FILE:
+            elif packet.status == Packet.END_FILE:
                 file_completo = b''.join(file)
                 #path = Path('C:\\Users\\utente\\Desktop\\ITIS\\TPSIT\\Python').joinpath('002_InvioFileUDP/results.pdf')
                 #path.write_file(file_completo)
                 write_file(file_completo)
-                break
 
 
 if __name__ == "__main__":
